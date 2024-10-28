a1f92a966186b7beb45a47e8ff0502dd8ef97bca
lschmidtdev <146366702+lschmidtdev@users.noreply.github.com>
2024-10-28 09:11:28 -0300

    Update cadastro_script.js

--- web_proj_plus_bootstrap_final/Cadastro/cadastro_script.js
+++ web_proj_plus_bootstrap_final/Cadastro/cadastro_script.js
@@ -1,14 +1,18 @@
 function validarCadastro(){
-    if (document.getElementById("name").innerText == null) {
+    if (document.getElementById("name").value == "") {
         alert("Nome não inserido!");
         return false;
-    } else if (document.getElementById("email").innerText == null) {
+    } else if (document.getElementById("email").value == "") {
         alert("Email não inserido!");
         return false;
     } else if(document.getElementById("password1").value != document.getElementById("password2").value){
         alert("As senhas não conferem!");
         return false;
-    } else {
+    } else if(document.getElementById("password1").value == "" ||document.getElementById("password2").value == "" )
+    {
+        alert("Senha não inserida!");
+    }
+     else {
         alert("Cadastro realizado com sucesso!")
         location.href = "/web_proj_plus_bootstrap/Home/Home.html"
     }
